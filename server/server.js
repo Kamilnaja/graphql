9dea9b44a2398d1a6ad03bb9df56a6f55dbc05e5
kamilnaja <kamilnaja@gmail.com>
2022-02-06 14:25:36 +0100

    working hello world example from graphql tut

--- server/server.js
+++ server/server.js
@@ -1,48 +1,25 @@
-const { ApolloServer, gql } = require("apollo-server-express");
-const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const express = require("express");
-const http = require("http");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
 
-async function startApolloServer(typeDefs, resolvers) {
-  const app = express();
-  const httpServer = http.createServer(app);
+const schema = buildSchema(`type Query {
+  hello: String
+}`);
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-  });
-
-  await server.start();
-  server.applyMiddleware({
-    app,
-    path: "/",
-  });
-
-  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-  console.log(`Server is running on ${server.graphqlPath}`);
-}
-
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const books = [
-  { title: "Czterej pancerni i pies", author: "Janusz Przymanowski" },
-  { title: "Przygody Hucka", author: "Mark Twain" },
-];
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
+const root = {
+  hello: () => `Hello world`,
 };
 
-startApolloServer(typeDefs, resolvers);
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(4000);
+console.log("Running a GraphQL API SERVER ON localhost:4000");
