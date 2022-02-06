dd87ee0492ee69bd13d5785575beee58503119e9
kamilnaja <kamilnaja@gmail.com>
2022-02-06 14:10:45 +0100

    working hello world example

--- server/server.js
+++ server/server.js
@@ -1,4 +1,27 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
+const express = require("express");
+const http = require("http");
+
+async function startApolloServer(typeDefs, resolvers) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/",
+  });
+
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`Server is running on ${server.graphqlPath}`);
+}
 
 const typeDefs = gql`
   type Book {
@@ -22,7 +45,4 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-server
-  .listen({ port: 9000 })
-  .then((serverInfo) => console.log(`🚀 Server running at ${serverInfo.url}`));
+startApolloServer(typeDefs, resolvers);
